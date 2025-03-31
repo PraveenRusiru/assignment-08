777644ed2fdd44fc9ce5fa2140dd57acb6674df9
praveenrusiru <praveenrusiru752@gmail.com>
2025-03-31 23:46:13 +0530

    emptied all  arrays

--- DB/Database.js
+++ DB/Database.js
@@ -2,12 +2,9 @@ import { CustomerDto } from "../Dto/CustomerDto.js";
 import { ItemDto } from "../Dto/ItemDto.js";
 
 export class Database {
-  Customers = new Array(new CustomerDto("C00-001", "Praveen", "Galle", 334343));
+  Customers = new Array();
 
-  Items = new Array(
-    new ItemDto("I00-001", "dfdf", 34, 456),
-    new ItemDto("I00-002", "ddffdf", 56, 5456)
-  );
+  Items = new Array( );
   Orders = new Array();
   OrderDetails = new Array();
 }
